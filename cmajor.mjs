57f1231cbef6e471feff58092abe2a2541c7cd63
nasuda <nasuda908@gmail.com>
2026-02-09 04:07:44 +0900

    rename

---
+++ cmajor.mjs
@@ -0,0 +1,20 @@
+const args = process.argv.slice(2);
+const cKey = args[0];
+//diatonic code
+
+const key = "C";
+const majorScale = ["c", "d", "e", "f", "g", "a", "b"];
+
+for (let i = 0; i < majorScale.length; i++ ) {
+  const degree = i+1;
+  const note = majorScale[i];
+
+  if (degree !== 1 && degree !== 4 && degree !== 5) {
+    const majorInterVal = "M" + degree.toString();
+    console.log(`interval: ${majorInterVal} note: ${note}`);
+  } else {
+
+    console.log(`"interval": ${degree} "note:" ${note}`);
+  }
+
+}
