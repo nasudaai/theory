a481b03e320c10195396d7ef8200615b06bc6a18
nasuda <nasuda908@gmail.com>
2026-02-09 06:01:16 +0900

    add create code

--- c-major7.mjs
+++ c-major7.mjs
@@ -1,6 +1,29 @@
+//sample
+/*
 const cMajor7 = {
   root: "C",
   M3rd: "E",
   P5th: "G",
   M7th: "B"
 };
+*/
+
+const cMajorScale = ["c", "d", "e", "f", "g", "a", "b"];
+const Cmajor7Note = [1, 3, 5, 7];
+
+const cMaj7 = { //cMaj7 -> cord
+  1: "",
+  3: "",
+  5: "",
+  7: ""
+};
+
+// create CM7 obj
+for (let i = 0; i < Cmajor7Note.length; i++) {
+  let key = "";
+  key = Cmajor7Note[i];
+  cMaj7[key] = cMajorScale[Cmajor7Note[i] - 1];
+}
+
+console.log(cMaj7);
+
