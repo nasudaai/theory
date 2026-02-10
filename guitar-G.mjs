e7f69f132f459db798006a7c282a7421c859b4b2
nasuda <nasuda908@gmail.com>
2026-02-10 10:24:24 +0900

    create pentatonic

--- guitar-G.mjs
+++ guitar-G.mjs
@@ -4,4 +4,11 @@ const STRINGSNote = ["e", "b", "g", "d", "a", "e"];
 //const fletNumber = [];
 
 
+const GmajorScale = ["g", "a", "b", "c", "d", "e", "f#"];
 
+const GPentatonic = [1, 2, 3, 5, 6].map( degree => {
+  const note = GmajorScale[degree - 1];
+  return note;
+});
+
+console.log(GPentatonic);
